bf90111fee3f9ce113161035c54787baed00cb23
niteeshmurthyBhat <87080121+niteeshmurthyBhat@users.noreply.github.com>
2021-07-07 19:43:56 +0530

    Add files via upload

---
+++ sparks/index.php
@@ -0,0 +1,31 @@
+<?php
+include 'includes/common.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Niteeshmurthy Bhat</title>
+</head>
+
+<body style="background-image: url('bank.jfif');   height: 100vh;
+    width: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;" >
+<?php
+include 'includes/header.php';
+?>
+
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
